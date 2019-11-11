443c5e2194b04dfdcab982c5d6284fe4166e1eb6
Tc <jackgoodchen2019@gmail.com>
2019-11-11 13:46:54 +0800

    upd

--- main.go
+++ main.go
@@ -1,27 +1,44 @@
-
-
 package main
 
 import (
-    "fmt"
+	"fmt"
 	"net/http"
+	"shop/models"
+	"shop/pkg/logging"
 	"shop/pkg/setting"
 	"shop/routers"
-	"shop/pkg/logging"
 )
 
-func main() {
-    router := routers.InitRouter()
-
-	logging.Info("server start")
-    s := &http.Server{
-        Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-        Handler:        router,
-        ReadTimeout:    setting.ReadTimeout,
-        WriteTimeout:   setting.WriteTimeout,
-        MaxHeaderBytes: 1 << 20,
-    }
-
-    s.ListenAndServe()
+func init() {
+	//加载配置参数
+	setting.Setup()
+	//加载数据库
+	models.Setup()
 }
 
+func main() {
+
+	//清理资源
+	defer func() {
+		models.CloseDB()
+	}()
+
+	routersInit := routers.InitRouter()
+
+	endPoint := fmt.Sprintf("%s:%d",
+		"localhost", setting.ServerCfg.HttpPort)
+
+	server := &http.Server{
+		Addr:           endPoint,
+		Handler:        routersInit,
+		ReadTimeout:    setting.ServerCfg.ReadTimeout,
+		WriteTimeout:   setting.ServerCfg.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+	fmt.Printf("[info] start http server listening %s", endPoint)
+	logging.Info("服务正常启动 listening:", endPoint)
+
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Printf("启动服务ERROR[%v]", err.Error())
+	}
+}
--- models/models.go
+++ models/models.go
@@ -1,57 +1,33 @@
-
-
 package models
 
 import (
-    "log"
-    "fmt"
-    "github.com/jinzhu/gorm"
-    _ "github.com/jinzhu/gorm/dialects/mysql"
-	"os"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"shop/pkg/logging"
+	"shop/pkg/setting"
 )
 
 var db *gorm.DB
 
-type Model struct {
-    ID int `gorm:"primary_key" json:"id"`
-    CreatedOn int `json:"created_on"`
-    ModifiedOn int `json:"modified_on"`
-}
+func Setup() {
+	var err error
 
-func init() {
-    var (
-        err error
-		dbType, dbName, user, password, host, tablePrefix string
-    )
+	if db, err = gorm.Open(setting.Dbcfg.DbType,
+		fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+			setting.Dbcfg.DbUser,
+			setting.Dbcfg.DbPass,
+			setting.Dbcfg.DbHost,
+			setting.Dbcfg.DbPort,
+			setting.Dbcfg.DbName)); err != nil {
+		logging.Fatal(err)
+	}
 
-	dbType = os.Getenv("DBTYPE")
-    dbName = os.Getenv("DBNAME")
-    user = os.Getenv("DBUSER")
-    password = os.Getenv("DBPASSWORD")
-    host = os.Getenv("DBHOST")
-    tablePrefix = os.Getenv("DBTABLE_PREFIX")
-
-    db, err = gorm.Open(dbType, fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", 
-        user, 
-        password, 
-        host, 
-        dbName))
-
-    if err != nil {
-        log.Println(err)
-    }
-
-    gorm.DefaultTableNameHandler = func (db *gorm.DB, defaultTableName string) string  {
-        return tablePrefix + defaultTableName;
-    }
-
-    db.SingularTable(true)
-    db.LogMode(true)
-    db.DB().SetMaxIdleConns(10)
-    db.DB().SetMaxOpenConns(100)
+	db.SingularTable(true)
+	db.DB().SetMaxIdleConns(10)
+	db.DB().SetMaxOpenConns(100)
 }
 
 func CloseDB() {
-    defer db.Close()
+	db.Close()
 }
-
